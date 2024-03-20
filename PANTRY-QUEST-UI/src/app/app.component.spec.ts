113e4107f7282480636fbcec1b287d55d282e774
Nikhilesh pavan Vaddi <nikhileshpavanvaddi@gmail.com>
2024-03-20 02:24:24 -0500

    Updated UI project to angular 16 to avoid middleware version conflicts

--- PANTRY-QUEST-UI/src/app/app.component.spec.ts
+++ PANTRY-QUEST-UI/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'PANTRY-QUEST' title`, () => {
+  it(`should have as title 'PANTRY-QUEST-UI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PANTRY-QUEST');
+    expect(app.title).toEqual('PANTRY-QUEST-UI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PANTRY-QUEST');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PANTRY-QUEST-UI app is running!');
   });
 });
